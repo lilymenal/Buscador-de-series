a4e93d5af922d4bcb8e4f4093a775ad47f107780
lilymenal <liliana.mendoza.alvear@gmail.com>
2021-02-09 17:22:37 +0100

    update intermedia structure

--- js/main.js
+++ js/main.js
@@ -2,40 +2,125 @@
 'use strict';
 
 const seriesElement = document.querySelector('.js-series');
+const buttonElement = document.querySelector('.js-button');
+const searchElement = document.querySelector('.js-text');
+const favoriteElement = document.querySelector('.js-favorites');
+
+let seriesShow = [];
+let favoritesSeries = [];
 
 // api
 
-//function callToApi() {
-    fetch('http://api.tvmaze.com/search/shows?q=girlmore')
+function callToApi(searchValue) {
+    fetch('http://api.tvmaze.com/search/shows?q='+ searchValue)
       .then(response => response.json())
       .then(data => {
-          //console.log (data[0].show);
-          paintSeries(data);
+          seriesShow = data;
+          paintSeries(seriesShow);
+          //console.log (seriesShow);
         });
-//}
+}
 
 // paint
 
 function paintSeries(characteres) {
     let htmlCode = '';
-          htmlCode += `<ul class = listClass>`;
+            htmlCode += `<ul class = listClass>`;
             for (let index = 0; index < characteres.length; index++) {
                 const elements = characteres[index];
-                const showElement = [elements.show];
-                //console.log (showElement);            
-            for (const itemsShow of showElement) {
-                //console.log (itemsShow.name);
-                //console.log (itemsShow.image);
-                htmlCode += '<li>';
-                htmlCode += `<img class="imagen" src="${itemsShow.image}">`;
-                htmlCode += `<p class="seriesname"> ${itemsShow.name}</p>`;        
+                const showElement = [elements.show];       
+            for (let itemsShow of showElement) {
+                htmlCode += '<li class="">';
+                htmlCode += `<button class="checked js-clicked" id="${itemsShow.id}">`;
+                const imageShow = itemsShow.image;
+                if (imageShow === null){
+                    htmlCode += `<img class="imagen" src="https://via.placeholder.com/210x295/ffffff/666666/?
+                    text=TV">`;
+                    } else {
+                        htmlCode += `<img class="imagen" src="${imageShow.medium}">`;
+                    }                                
+                htmlCode += `<p class="seriesname"> ${itemsShow.name}</p>`;   
+                htmlCode += '</buton>';    
                 htmlCode += '</li>';
             }
             }
             htmlCode += `</ul>`;
             seriesElement.innerHTML = htmlCode;
+            
+            listenFavoritesSeries();
   }
-console.log (seriesElement);
+
+ //search series
+
+function handleSearch() {
+        callToApi(searchElement.value);
+        setInLocalStorage(searchElement.value);
+}
+buttonElement.addEventListener('click',handleSearch);
 
 
-  
+//check favorites
+
+function listenFavoritesSeries() {
+    const clickedElements = document.querySelectorAll('.js-clicked');
+            for (const clickedElement of clickedElements) {
+                clickedElement.addEventListener('click',handleFavorites);
+                //console.log (clickedElement);
+            }
+}
+
+function handleFavorites(ev) {
+    let listen = ev.currentTarget;
+    console.log ('escucha', listen.id);
+    
+    const data = seriesShow.find (select => (select.show.id) = parseInt(listen.id));
+    console.log (data);
+    favoritesSeries.push (data);
+        //htmlFavoriteCode += `<ul class = listFavoriteClass>`;     
+            for (const favoriteSerie of favoritesSeries) {
+                console.log (favoriteSerie);
+            }
+                //console.log (itemsShow.name);
+                //console.log (itemsShow.image);
+                /*htmlFavoriteCode += '<li class="">';
+                htmlFavoriteCode += `<button class="checked js-clicked id="${itemsShow.id}">`;
+                const imageShow = [itemsShow.image];
+                for (const image  of imageShow ) {
+                    //console.log(image.medium)
+                
+                //if (imageShow == null){
+                    htmlFavoriteCode += `<img class="imagen" src="https://via.placeholder.com/210x295/ffffff/666666/?
+                    text=TV">`;
+                    /*} else {
+                        htmlCode += `<img class="imagen" src="./logo-adalab.png">`;
+                    }  
+                }                              
+                htmlFavoriteCode += `<p class="seriesname"> ${itemsShow.name}</p>`;   
+                htmlFavoriteCode += '</buton>';    
+                htmlFavoriteCode += '</li>';
+            }
+            }
+            htmlFavoriteCode += `</ul>`;*/
+
+    //favoriteElement.innerHTML = htmlFavoriteCode;*/
+}
+ 
+ //local storage
+
+ function setInLocalStorage(searchSeries) {
+   localStorage.setItem ('searchvalue', searchSeries);
+ };
+
+ function getFromLocalStorage() {
+    const searchText= localStorage.getItem ('searchvalue'); 
+    if (searchText === null){
+        callToApi ('friends');
+        searchElement.value = 'friends';
+    } else {
+        callToApi(searchText);
+        searchElement.value = searchText;
+    }
+ };
+
+ // start app: search by friends
+ getFromLocalStorage();
